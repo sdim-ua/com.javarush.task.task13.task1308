8b05511f133b238c6913eb3c76708265b902aa85
sdim-ua <ingersol@yandex.ua>
2019-06-23 21:19:18 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,16 @@
+/* 
+Эй, ты там живой?
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+    
+    interface Person{
+        boolean isAlive();
+    }
+    
+    interface Presentable extends Person {
+        
+    }
+}
